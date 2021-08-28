371664ad6f965a4e9e4efa5d7d12c8f53308b173
iterus <iterus@protonmail.com>
2021-08-28 11:35:35 +0300

    Add bluetooth and mi band specific UUID constants

---
+++ common/src/commonMain/kotlin/xyz/iterus/meband/bluetooth/BleConst.kt
@@ -0,0 +1,6 @@
+package xyz.iterus.meband.bluetooth
+
+internal object BleConst {
+
+    const val BASE_UUID = "0000-1000-8000-00805f9b34fb"
+}
---
+++ common/src/commonMain/kotlin/xyz/iterus/meband/bluetooth/MiBandConst.kt
@@ -0,0 +1,10 @@
+package xyz.iterus.meband.bluetooth
+
+import xyz.iterus.meband.bluetooth.BleConst.BASE_UUID
+
+internal object MiBandConst {
+
+    const val BASE_MI_UUID = "0000-3512-2118-0009af100700"
+
+    const val SERVICE_MI_BAND = "0000FEE0-$BASE_UUID"
+}
