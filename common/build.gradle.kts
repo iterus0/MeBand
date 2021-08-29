fa6efa5158e52e59b47c2e8a66001394f0d796b4
iterus <iterus@protonmail.com>
2021-08-29 21:54:24 +0300

    Update kable 0.8.0 -> 0.9.1

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -32,7 +32,7 @@ kotlin {
 
     sourceSets {
         val coroutinesVersion = "1.5.1"
-        val kableVersion = "0.8.0"
+        val kableVersion = "0.9.1"
         val ktorIoVersion = "1.6.3"
 
         val commonMain by getting {
