ed62e475b7b8f9e0bb4617f44f53872f35007dfe
iterus <iterus@protonmail.com>
2021-08-31 08:35:23 +0300

    Add test activity

--- androidApp/src/main/kotlin/xyz/iterus/meband/android/MainActivity.kt
+++ androidApp/src/main/kotlin/xyz/iterus/meband/android/MainActivity.kt
@@ -1,16 +1,85 @@
 package xyz.iterus.meband.android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.bluetooth.BluetoothManager
+import android.content.Context
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.juul.kable.Peripheral
+import com.juul.kable.peripheral
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import xyz.iterus.meband.MiBand
+import xyz.iterus.meband.battery.MiBatteryInfoService
+import xyz.iterus.meband.event.MiDeviceEventService
+import xyz.iterus.meband.heartrate.GenericBleHeartRateService
+import xyz.iterus.meband.music.MiMusicControlService
+import java.util.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private lateinit var miband: Peripheral
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        val batteryTv: TextView = findViewById(R.id.batteryLevel)
+        val heartrateTv: TextView = findViewById(R.id.heartrate)
+
+        lifecycleScope.launchWhenStarted {
+            println("[Job] Started")
+            val mibandId = "XX:XX:XX:XX:XX:XX"
+            val bleManager =
+                applicationContext.getSystemService(Context.BLUETOOTH_SERVICE) as? BluetoothManager
+            val adapter = bleManager?.adapter
+                ?: throw UnsupportedOperationException("Bluetooth is not supported!")
+
+            val btd = adapter.getRemoteDevice(mibandId)
+            miband = peripheral(btd) {
+                logging {
+                    //level = Logging.Level.Events
+                }
+            }
+            miband.connect()
+
+            val batInfoService = MiBatteryInfoService(miband)
+            val hrService = GenericBleHeartRateService(miband)
+            val deviceEventService = MiDeviceEventService(miband, lifecycleScope)
+            val musicControlService = MiMusicControlService(miband, lifecycleScope, deviceEventService)
+            val mbs = MiBand(miband, batInfoService, hrService, musicControlService)
+
+            batteryTv.text = "${mbs.batteryLevel()} %"
+
+            val hr = async {
+                mbs.heartrate()
+                    .collect {
+                        println("[HR] $it")
+                        heartrateTv.text = "$it BPM"
+                    }
+            }
+
+            val mus = async {
+                mbs.musicEvent()
+                    .collect {
+                        println("[Music] $it")
+                        heartrateTv.text = "$it"
+                    }
+            }
+
+            hr.await()
+            mus.await()
+        }.invokeOnCompletion {
+            println("[Job] Completed")
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        lifecycleScope.launch {
+            miband.disconnect()
+        }
     }
 }
